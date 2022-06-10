50fb63170e1f9ad7ff6c673f092d807c8b6296e9
Kivi Shapiro <github.com@kivis.net>
2022-06-09 22:30:34 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function updateText() {
-    let yourPlace = document.getElementById("your-famous-place").value;
-    document.getElementById("places").innerText = yourPlace;
-
-    for (let hiddenText of document.getElementsByClassName("noseeum")) {
-        hiddenText.style.visibility="visible";
-    }
-}
